7aae19140e30590cc3a887820bac05f6e44dad1a
W. Max Lees <max.lees@gmail.com>
2016-04-07 13:28:00 -0600

    Created basic memory manager

--- src/main/java/cse/ucdenver/csci5593/memory/MemoryManager.java
+++ src/main/java/cse/ucdenver/csci5593/memory/MemoryManager.java
@@ -1,5 +1,8 @@
 package cse.ucdenver.csci5593.memory;
 
+import cse.ucdenver.csci5593.memory.exceptions.AddressNotFoundException;
+
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -11,6 +14,7 @@ public class MemoryManager {
      * lower indices first)
      */
     private List<MemoryModule> modules;
+    private HashMap<Integer, Integer> values;
 
     /**
      * Add a module to the memory manager
@@ -29,6 +33,60 @@ public class MemoryManager {
         boolean result = (this.modules.get(index) != null);
 
         this.modules.set(index, module);
+
+        return result;
+    }
+
+    public void update() {
+        for (MemoryModule module : modules) {
+            module.update();
+        }
+    }
+
+    /**
+     * Set the value of a given memory location
+     *
+     * @param address The memory location to set
+     * @param value The value to save to the memory location
+     */
+    public void setMemoryValue(int address, int value) {
+        for (MemoryModule module : modules) {
+            module.setValue(address);
+        }
+
+        this.values.put(address, value);
+    }
+
+    public class MemoryReturn {
+        public int accessTime;
+        public int value;
+    }
+
+    /**
+     * Get the value of a given memory address
+     *
+     * @param address The address to access
+     * @return A MemoryReturn object containing the value and
+     *         the time it took to access the given value
+     * @throws AddressNotFoundException
+     */
+    public MemoryReturn getMemoryValue(int address) throws AddressNotFoundException {
+        MemoryReturn result = new MemoryReturn();
+
+        if (values.containsKey(address)) {
+            result.value = values.get(address);
+        } else {
+            throw new AddressNotFoundException();
+        }
+
+        int accum = 0;
+        for (MemoryModule module : modules) {
+            accum += module.checkTime();
+            if (module.hasValue(address)) {
+                result.accessTime = accum;
+                break;
+            }
+        }
 
         return result;
     }
---
+++ src/main/java/cse/ucdenver/csci5593/memory/exceptions/AddressNotFoundException.java
@@ -0,0 +1,6 @@
+package cse.ucdenver.csci5593.memory.exceptions;
+
+/**
+ * Created by max on 4/7/16.
+ */
+public class AddressNotFoundException extends RuntimeException {}
