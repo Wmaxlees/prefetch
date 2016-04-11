7adfe4b7f62d48b9ddd5a4907dc993fb9a766c8f
JASPREET KAUR <er.jaspreet91@gmail.com>
2016-04-11 13:00:54 -0600

    Update MemoryModule.java

--- src/main/java/cse/ucdenver/csci5593/memory/MemoryModule.java
+++ src/main/java/cse/ucdenver/csci5593/memory/MemoryModule.java
@@ -1,40 +1,74 @@
 package cse.ucdenver.csci5593.memory;
+import java.util.HashMap;
+import java.util.Set;
+public L1Cache implements  MemoryModule {
+   
+   public HashMap<Integer, String> hmap; 
 
-/**
- * Created by max on 3/14/16.
- */
-public interface MemoryModule {
-    /**
-     * Returns true if the memory module holds the value
-     * of the given index
-     *
-     * @param memoryLocation The index of the memory needed
-     * @return true if the module holds the value, false otherwise
-     */
-    boolean hasValue(int memoryLocation);
+	public boolean hasValue(int memoryLocation) {
+		hmap= new HashMap<Integer, String>();
+		
+	 if( hmap.containsKey(memoryLocation))
+	 {
+		 System.out.println("value is "+hasValue(memoryLocation)); 
+	 }
+	 else
+	 {
+		 System.out.println( "the module does not holds the value");
+		
+	 }
+	 
+	return false;	
+	}
+    
+    void setValue(int memoryLocation)
+{
+   hmap= new HashMap<Integer, String>();
+		  hmap.put(1, "one");
+	      hmap.put(2, "two");
+	      hmap.put(3, "three");
+	      hmap.put(4, "four");
+	      hmap.put(5, "five");
+	      hmap.put(6, "six");
+	      hmap.put(7, "seven");
+	      hmap.put(8, "eight");
+	      hmap.put(9, "nine");
+	      hmap.put(10, "ten");
+	      Set set = hmap.entrySet();
+	      System.out.println(hmap);
+}
+    void update()
+    {
+        hmap= new HashMap<Integer, String>();
+		 hmap.put(1, "a");
+	      hmap.put(2, "b");
+	      hmap.put(3, "c");
+	      hmap.put(4, "d");
+	      hmap.put(5, "e");
+	      hmap.put(6, "f");
+	      hmap.put(7, "g");
+	      hmap.put(8, "h");
+	      hmap.put(9, "i");
+	      hmap.put(10, "j");
+	      System.out.println(hmap);
+		
+    }
 
-    /**
-     * Sets the module to hold the value of the given
-     * memory location
-     *
-     * @param memoryLocation
-     */
-    void setValue(int memoryLocation);
-
-    /**
-     * Must be called every tick. Used for when caches update
-     * flags based on ticks, etc.
-     */
-    void update();
-
-    /**
-     * @return The time it takes to check whether the module contains
-     * memory
-     */
+   
     int checkTime();
 
-    /**
-     * @return The access time of the module
-     */
-    int accessTime();
+  
+    int accessTime()
+    {
+        
+       long startTime = System.currentTimeMillis();
+        long total=0;
+        for (int i=0;i<10000000;i++)
+        {
+        	total += i;
+        }
+        long stopTime= System.currentTimeMillis();
+        long elapsedTime = stopTime-startTime;
+        System.out.println(elapsedTime);
+    }
 }
