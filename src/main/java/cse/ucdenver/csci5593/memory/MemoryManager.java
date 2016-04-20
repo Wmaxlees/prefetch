b5fff5c851b57d0b874c96f2c86b1d18705323dc
W. Max Lees <max.lees@gmail.com>
2016-04-19 19:45:44 -0600

    Fixed errors

--- src/main/java/cse/ucdenver/csci5593/memory/MemoryManager.java
+++ src/main/java/cse/ucdenver/csci5593/memory/MemoryManager.java
@@ -14,9 +14,9 @@ public class MemoryManager {
      * Holds the memory modules (attempts to access
      * lower indices first)
      */
-    protected RegisterMemoryModule registers;
-    protected List<MemoryModule> modules;
-    protected HashMap<Integer, Integer> values;
+    private RegisterMemoryModule registers;
+    private List<MemoryModule> modules;
+    private HashMap<Integer, Integer> values;
 
     /**
      * Create a new instance of the MemoryManager
@@ -33,6 +33,7 @@ public class MemoryManager {
      */
     public void setRegisterMemoryModule(RegisterMemoryModule rmm) {
         this.registers = rmm;
+        this.setMemoryValue(this.registers.getRegisterAddress("%ip"), this.registers.getMaxRegisterIndex() + 1);
     }
 
     /**
@@ -43,6 +44,16 @@ public class MemoryManager {
      */
     public int getRegisterAddress(String name) {
         return this.registers.getRegisterAddress(name);
+    }
+
+    /**
+     * Get the status of a flag
+     *
+     * @param name Name of the flag
+     * @return The status of the flag
+     */
+    public boolean getFlagStatus(RegisterMemoryModule.Flag name) {
+        return this.registers.getFlag(name);
     }
 
     /**
