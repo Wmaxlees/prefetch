5c61dbd5be67e122260eade31f43f03762438553
AhmedAldhlaki <ahmedrahoam@gmail.com>
2016-04-19 18:30:52 -0600

    Made JMP Instruction

---
+++ src/main/java/cse/ucdenver/csci5593/instruction/x86/InstJMP.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Ahmed on 4/19/16.
+ */
+
+package cse.ucdenver.csci5593.instruction.x86;
+
+import cse.ucdenver.csci5593.instruction.BadlyFormattedInstructionException;
+import cse.ucdenver.csci5593.instruction.Instruction;
+import cse.ucdenver.csci5593.instruction.x86.helpers.IPHelper;
+import cse.ucdenver.csci5593.memory.MemoryManager;
+
+public class InstJMP extends Instruction {
+	@Override
+	public int CPI(MemoryManager memoryManager)
+			throws BadlyFormattedInstructionException {
+		return 1;
+	}
+	
+	public String opCode(){
+		return "JMP";
+	}
+	
+	public int execute (MemoryManager memoryManager) throws BadlyFormattedInstructionException{
+		if (this.operands.size() != 1){
+			throw new BadlyFormattedInstructionException(this.opCode()+" Incorrect number of arguments for JMP.");
+	}
+		
+	    IPHelper.setIP(memoryManager, this.operands.get(0).getValue());
+	    return 0;
+	}	
+}
+
+
+
+ 
+
+
