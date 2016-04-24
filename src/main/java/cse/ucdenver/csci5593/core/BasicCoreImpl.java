3cfc83f7e9bd7d7031cd6f2f9e8b57bb989e9c3a
AhmedAldhlaki <ahmedrahoam@gmail.com>
2016-04-24 01:45:49 -0600

    Fixing the comments in the class BasicCoreImple

--- src/main/java/cse/ucdenver/csci5593/core/BasicCoreImpl.java
+++ src/main/java/cse/ucdenver/csci5593/core/BasicCoreImpl.java
@@ -9,57 +9,54 @@ import java.util.HashMap;
  * Created by Ahmed Aldhlaki on 4/1/16.
  */
 
-public class BasicCoreImpl implements Core
-{
+public class BasicCoreImpl implements Core {
     /**
-     * Declare the variable that we need 
+     * Declare the variable that we need
      */
     MemoryManager mm;
-	int currentCycle;
-	int currentInstCycle;
-	Instruction currentInst;
-	HashMap<Integer, Instruction> inst;
-	
-	/**
-     * set-up the instruction 
+    int currentCycle;
+    int currentInstCycle;
+    Instruction currentInst;
+    HashMap<Integer, Instruction> inst;
+
+    /**
+     * set-up the instruction
      */
-	public void setInstruction(HashMap<Integer, Instruction> list)
-	{
-		this.inst = list;
-	}
-	
-	/**
-     * Update the current cycle by adding one each time
-     *  Updating the current Instruction cycle by subtracting one each time and check it if it is zero or not
+    public void setInstruction(HashMap<Integer, Instruction> list) {
+        this.inst = list;
+    }
+
+    /**
+     * Update the current cycle by adding one each time Updating the current
+     * Instruction cycle by subtracting one each time and check it if it is zero
+     * or not
      */
-	public boolean update()
-	{
-		++this.currentCycle;
-		--this.currentInstCycle;
-		if (this.currentInstCycle == 0)
-		{
-			this.currentInst.execute(this.mm);
-			this.currentInst = inst.get(mm.getMemoryValue(mm.getRegisterAddress("%ip")));
+    public boolean update() {
+        ++this.currentCycle;
+        --this.currentInstCycle;
+        if (this.currentInstCycle == 0) {
+            this.currentInst.execute(this.mm);
+            this.currentInst = inst.get(mm.getMemoryValue(mm
+                    .getRegisterAddress("%ip")));
             if (this.currentInst == null) {
                 return false;
             }
-		}
+        }
         return true;
-	}
-		
-	/**
+    }
+
+    /**
      * Returning the Run Time for current Cycle
      */
-	
-	public int getRuntime()
-	{
-		return this.currentCycle;
-	}
-	
-	/**
+
+    public int getRuntime() {
+        return this.currentCycle;
+    }
+
+    /**
      * Setting the Memory Manager
      */
-	public void setMemoryManager(MemoryManager mm) {
-		this.mm = mm;
-	}
+    public void setMemoryManager(MemoryManager mm) {
+        this.mm = mm;
+    }
 }
