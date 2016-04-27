d653a8faf9d3d19320cc34926d99cade8395ef92
jaspreet kaur <er.jaspreet91@gmail.com>
2016-04-27 15:57:03 -0600

    OR

--- src/main/java/cse/ucdenver/csci5593/instruction/x86/InstOr.java
+++ src/main/java/cse/ucdenver/csci5593/instruction/x86/InstOr.java
@@ -8,6 +8,7 @@ import cse.ucdenver.csci5593.instruction.x86.helpers.IPHelper;
 import cse.ucdenver.csci5593.memory.MemoryManager;
 import cse.ucdenver.csci5593.memory.RegisterMemoryModule;
 import cse.ucdenver.csci5593.parser.X86InstructionSet;
+
 /**
  * Created by Jaspreet.
  */
@@ -27,7 +28,7 @@ public class InstOr extends Instruction {
 
         long result = this.getOperand(0).getValue(memoryManager) | this.getOperand(1).getValue(memoryManager);
 
-        memoryManager.setMemoryValue(this.getOperand(1).getAddress(memoryManager), (int)result);
+        memoryManager.setMemoryValue(this.getOperand(1).getAddress(memoryManager), (int) result);
 
         this.setFlags(memoryManager, result);
 
