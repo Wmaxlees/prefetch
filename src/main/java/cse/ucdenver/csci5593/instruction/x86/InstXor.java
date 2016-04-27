a1c3b9b02a17fddc9c0d29add2b6683790da500e
jaspreet kaur <er.jaspreet91@gmail.com>
2016-04-27 16:20:31 -0600

    xor

--- src/main/java/cse/ucdenver/csci5593/instruction/x86/InstXor.java
+++ src/main/java/cse/ucdenver/csci5593/instruction/x86/InstXor.java
@@ -7,7 +7,9 @@ import cse.ucdenver.csci5593.instruction.x86.helpers.IPHelper;
 import cse.ucdenver.csci5593.memory.MemoryManager;
 import cse.ucdenver.csci5593.memory.RegisterMemoryModule;
 import cse.ucdenver.csci5593.parser.X86InstructionSet;
-
+/**
+ * Created by Jaspreet.
+ */
 public class InstXor extends Instruction {
     public int CPI(MemoryManager memoryManager) throws BadlyFormattedInstructionException {
         return 2;
