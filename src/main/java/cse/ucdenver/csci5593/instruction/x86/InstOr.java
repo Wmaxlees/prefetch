b85304a180e265a21cba86919cafc63a4babac62
jaspreet kaur <er.jaspreet91@gmail.com>
2016-04-27 15:56:15 -0600

    OR

--- src/main/java/cse/ucdenver/csci5593/instruction/x86/InstOr.java
+++ src/main/java/cse/ucdenver/csci5593/instruction/x86/InstOr.java
@@ -8,7 +8,9 @@ import cse.ucdenver.csci5593.instruction.x86.helpers.IPHelper;
 import cse.ucdenver.csci5593.memory.MemoryManager;
 import cse.ucdenver.csci5593.memory.RegisterMemoryModule;
 import cse.ucdenver.csci5593.parser.X86InstructionSet;
-
+/**
+ * Created by Jaspreet.
+ */
 public class InstOr extends Instruction {
     public int CPI(MemoryManager memoryManager) throws BadlyFormattedInstructionException {
         return 2;
